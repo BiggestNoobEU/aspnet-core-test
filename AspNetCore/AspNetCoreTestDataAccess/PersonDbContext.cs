fdc817d4a6442071cb678da2c415e5e34d6e6972
BiggestNoobEU <davis.simanis.rs@gmail.com>
2020-07-14 19:46:01 +0300

    person and person Db added

---
+++ AspNetCore/AspNetCoreTestDataAccess/PersonDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTestDataAccess
+{
+    class PersonDbContext : DbContext // = DataBase...this is our DB
+    {
+        public DbSet<Person> People { get; set; } // = table
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Initial Catalog=PersonTestDb;Integrated Security=True;");
+        }
+    }
+}
